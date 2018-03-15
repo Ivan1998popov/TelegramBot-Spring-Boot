020189a8cfc7dca736d1b627ccceefab7ab3daea
Ivan <Ivan@example.com>
2018-03-15 19:31:53 +0300

    create class Bot

--- src/main/java/com/telegram/springboot/Application.java
+++ src/main/java/com/telegram/springboot/Application.java
@@ -15,55 +15,20 @@ import java.util.Date;
 
 
 @SpringBootApplication
-public class Application extends TelegramLongPollingBot{
+public class Application {
 
 	public static void main(String[] args) {
 		ApiContextInitializer.init();
 		TelegramBotsApi telegramBotsApi =new TelegramBotsApi();
 
 		try{
-			telegramBotsApi.registerBot(new Application());
+			telegramBotsApi.registerBot(new Bot());
 		} catch (TelegramApiException e){
 			e.printStackTrace();
 		}
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(Bot.class, args);
 	}
 
 
-	@Override
-	public void onUpdateReceived(Update update) {
-		Message message =update.getMessage();
-		if (message!=null&&message.hasText()){
-			if(message.getText().equals("/time_now")){
-				Date date = new Date();
-				SimpleDateFormat formatForDateNow = new SimpleDateFormat("E dd.MM.yyyy 'и время' hh:mm:ss a zzz");
-				sendMsg(message,"Привет, сегодня "+formatForDateNow.format(date));
-			}else{
-				sendMsg(message,"Я тестовый бот");
-			}
-		}
-	}
 
-	private void sendMsg(Message message, String s) {
-		SendMessage sendMessage =new SendMessage();
-		sendMessage.enableMarkdown(true);
-		sendMessage.setChatId(message.getChatId().toString());
-		sendMessage.setReplyToMessageId(message.getMessageId());
-		sendMessage.setText(s);
-		try{
-			sendMessage(sendMessage);
-		}catch (TelegramApiException e){
-			e.printStackTrace();
-		}
-	}
-
-	@Override
-	public String getBotUsername() {
-		return "Ivan777_bot";
-	}
-
-	@Override
-	public String getBotToken() {
-		return "555686449:AAH9vle-Bbj-jYeWSArxeiDa_lvL0vb1h_0";
-	}
 }
