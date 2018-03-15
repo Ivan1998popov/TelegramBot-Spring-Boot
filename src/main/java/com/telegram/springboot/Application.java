a0450af585e3eb07bbe7a578a0db4693dfe4dccd
Ivan <Ivan@example.com>
2018-03-15 19:35:23 +0300

    class Aplication

--- src/main/java/com/telegram/springboot/Application.java
+++ src/main/java/com/telegram/springboot/Application.java
@@ -4,14 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
-import org.telegram.telegrambots.api.methods.send.SendMessage;
-import org.telegram.telegrambots.api.objects.Message;
-import org.telegram.telegrambots.api.objects.Update;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 
 @SpringBootApplication
@@ -26,7 +20,7 @@ public class Application {
 		} catch (TelegramApiException e){
 			e.printStackTrace();
 		}
-		SpringApplication.run(Bot.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 
