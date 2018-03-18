1cfaa4dc68b354ce63d59f746217217c61594da4
Ivan <Ivan@example.com>
2018-03-18 12:05:31 +0300

    change database 1.0

--- src/main/java/com/telegram/springboot/Application.java
+++ src/main/java/com/telegram/springboot/Application.java
@@ -1,11 +1,15 @@
 package com.telegram.springboot;
 
+import org.h2.tools.DeleteDbFiles;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
+import java.sql.SQLException;
+
+import static com.telegram.springboot.Schedule.insertWithStatement;
 
 
 @SpringBootApplication
@@ -13,6 +17,14 @@ public class Application {
 
 	public static void main(String[] args) {
 		ApiContextInitializer.init();
+		try {
+			// delete the H2 database named 'test' in the user home directory
+			DeleteDbFiles.execute("~", "tbl_schedule", true);
+			insertWithStatement();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		TelegramBotsApi telegramBotsApi =new TelegramBotsApi();
 
 		try{
--- src/main/java/com/telegram/springboot/Schedule.java
+++ src/main/java/com/telegram/springboot/Schedule.java
@@ -2,53 +2,61 @@ package com.telegram.springboot;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.sql.*;
 
 @Entity
 @Table(name = "tbl_schedule")
 public class Schedule {
 
-    private String group_;
-    private String day_;
-    private String schedule;
+    private static final String DB_DRIVER = "org.h2.Driver";
+    private static final String DB_CONNECTION = "jdbc:h2:~/tbl_schedule";
+    private static final String DB_USER = "";
+    private static final String DB_PASSWORD = "";
+    public static void insertWithStatement() throws SQLException {
+        Connection connection = getDBConnection();
+        Statement stmt = null;
+        try {
+            connection.setAutoCommit(false);
+            stmt = connection.createStatement();
+            stmt.execute("CREATE TABLE TBL_SCHEDULE(group_ VARCHAR(9), data DATE,schedule TEXT)");
+            stmt.execute("INSERT INTO TBL_SCHEDULE(group_, data,schedule) VALUES('КТбо2-2', '2018-03-19','Вакансия ВКСиРЭБ -.-. " +
+                    "ВП (лек.) Г-242	Вакансия ВКСиРЭБ -.-. " +
+                    "ВП (лек.) Г-242	Вакансия ВКСиРЭБ -.-. " +
+                    "ВП (лек.) Г-242	Вакансия ВКСиРЭБ -.-. " +
+                    "ВП (лек.) Г-242	Вакансия ВКСиРЭБ -.-. " +
+                    "ВП (лек.) Г-242')");
 
-    public Schedule(){
 
-    }
-    public Schedule (String group,String day,String schedule){
-        this.group_=group;
-        this.day_=day;
-        this.schedule=schedule;
-    }
-
-    public String getGroup() {
-        return group_;
-    }
-
-    public void setGroup(String group) {
-        this.group_ = group;
-    }
-
-    public String getDay() {
-        return day_;
+//            ResultSet rs = stmt.executeQuery("select * from PERSON");
+//            System.out.println("H2 Database inserted through Statement");
+//            while (rs.next()) {
+//                System.out.println("Id "+rs.getInt("id")+" Name "+rs.getString("name"));
+//            }
+            stmt.close();
+            connection.commit();
+        } catch (SQLException e) {
+            System.out.println("Exception Message " + e.getLocalizedMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            connection.close();
+        }
     }
 
-    public void setDay(String day) {
-        this.day_ = day;
-    }
-
-    public String getSchedule() {
-        return schedule;
-    }
-
-    public void setSchedule(String schedule) {
-        this.schedule = schedule;
-    }
-    @Override
-    public String toString() {
-        return "Schedule{" +
-                "group=" + group_ +
-                ", day='" + day_ + '\'' +
-                ", schedule=" + schedule +
-                '}';
+    private static Connection getDBConnection() {
+        Connection dbConnection = null;
+        try {
+            Class.forName(DB_DRIVER);
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER,
+                    DB_PASSWORD);
+            return dbConnection;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return dbConnection;
     }
 }
