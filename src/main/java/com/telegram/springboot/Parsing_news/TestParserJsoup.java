ad4cd301ad186c4534ad6481bebba4702c5ce9dc
Ivan <you@example.com>
2018-04-24 20:19:41 +0300

    ЮФУ parsing news content jsoup (1.11.2)

--- src/main/java/com/telegram/springboot/Parsing_news/TestParserJsoup.java
+++ src/main/java/com/telegram/springboot/Parsing_news/TestParserJsoup.java
@@ -16,20 +16,28 @@ public class TestParserJsoup {
     public static void main(String[] args) throws IOException {
 
         List<DataParse> dataParses =new ArrayList<>();
+        List<String> content =new ArrayList<>();
 
         Document doc = Jsoup.connect("http://priem.tti.sfedu.ru/").get();
         Elements liElements = doc.getElementsByAttributeValue("class","mod-articles-category-title ");
+
+        Elements pElements=doc.getElementsByAttributeValue("class","mod-articles-category-introtext");
 
         liElements.forEach(liElement->{
             Element aElement=liElement;
             String url=aElement.attr("href");
             String title = aElement.text();
-
             dataParses.add(new DataParse(url,title));
         });
 
-        dataParses.forEach(System.out::println);
+        pElements.forEach(pElement->{
+            Element aElement =pElement;
+            content.add(aElement.text());
 
+        });
+
+        dataParses.forEach(System.out::println);
+        content.forEach(System.out::println);
     }
 
 
