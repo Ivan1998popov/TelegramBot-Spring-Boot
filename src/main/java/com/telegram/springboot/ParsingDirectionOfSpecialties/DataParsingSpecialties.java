f59bf4729a321d5fc55881371595b5b1698a065a
Ivan <you@example.com>
2018-05-04 23:43:27 +0300

    ЮФУ parsing bakalavriat-specialitet finish jsoup (1.11.2)

--- src/main/java/com/telegram/springboot/ParsingDirectionOfSpecialties/DataParsingSpecialties.java
+++ src/main/java/com/telegram/springboot/ParsingDirectionOfSpecialties/DataParsingSpecialties.java
@@ -3,22 +3,15 @@ package com.telegram.springboot.ParsingDirectionOfSpecialties;
 public class DataParsingSpecialties {
 
     public String name_spec;
-    public String subject_one;
-    public String subject_two;
-    public String subject_three;
-    public String subject_one_bal;
-    public String subject_two_bal;
-    public String subject_three_bal;
+    public String subjects;
+    public String marks;
+    public String structural;
 
-    public DataParsingSpecialties(String name_spec, String subject_one, String subject_two, String subject_three,
-                                  String subject_one_bal, String subject_two_bal, String subject_three_bal) {
+    public DataParsingSpecialties(String name_spec, String subjects, String marks, String structural) {
         this.name_spec = name_spec;
-        this.subject_one = subject_one;
-        this.subject_two = subject_two;
-        this.subject_three = subject_three;
-        this.subject_one_bal = subject_one_bal;
-        this.subject_two_bal = subject_two_bal;
-        this.subject_three_bal = subject_three_bal;
+        this.subjects = subjects;
+        this.marks = marks;
+        this.structural = structural;
     }
 
 
@@ -30,64 +23,37 @@ public class DataParsingSpecialties {
         this.name_spec = name_spec;
     }
 
-    public String getSubject_one() {
-        return subject_one;
+    public String getSubjects() {
+        return subjects;
     }
 
-    public void setSubject_one(String subject_one) {
-        this.subject_one = subject_one;
+    public void setSubjects(String subjects) {
+        this.subjects = subjects;
     }
 
-    public String getSubject_two() {
-        return subject_two;
+    public String getMarks() {
+        return marks;
     }
 
-    public void setSubject_two(String subject_two) {
-        this.subject_two = subject_two;
+    public void setMarks(String marks) {
+        this.marks = marks;
     }
 
-    public String getSubject_three() {
-        return subject_three;
+    public String getStructural() {
+        return structural;
     }
 
-    public void setSubject_three(String subject_three) {
-        this.subject_three = subject_three;
-    }
-
-    public String getSubject_one_bal() {
-        return subject_one_bal;
-    }
-
-    public void setSubject_one_bal(String subject_one_bal) {
-        this.subject_one_bal = subject_one_bal;
-    }
-
-    public String getSubject_two_bal() {
-        return subject_two_bal;
-    }
-
-    public void setSubject_two_bal(String subject_two_bal) {
-        this.subject_two_bal = subject_two_bal;
-    }
-
-    public String getSubject_three_bal() {
-        return subject_three_bal;
-    }
-
-    public void setSubject_three_bal(String subject_three_bal) {
-        this.subject_three_bal = subject_three_bal;
+    public void setStructural(String structural) {
+        this.structural = structural;
     }
 
     @Override
     public String toString() {
         return "DataParsingSpecialties{" +
                 "name_spec='" + name_spec + '\'' +
-                ", subject_one='" + subject_one + '\'' +
-                ", subject_two='" + subject_two + '\'' +
-                ", subject_three='" + subject_three + '\'' +
-                ", subject_one_bal='" + subject_one_bal + '\'' +
-                ", subject_two_bal='" + subject_two_bal + '\'' +
-                ", subject_three_bal='" + subject_three_bal + '\'' +
+                ", subjects='" + subjects + '\'' +
+                ", marks='" + marks + '\'' +
+                ", structural='" + structural + '\'' +
                 '}';
     }
 }
--- src/main/java/com/telegram/springboot/ParsingDirectionOfSpecialties/TestParserSpecialties.java
+++ src/main/java/com/telegram/springboot/ParsingDirectionOfSpecialties/TestParserSpecialties.java
@@ -1,10 +1,10 @@
 package com.telegram.springboot.ParsingDirectionOfSpecialties;
 
-import com.telegram.springboot.Parsing_news.DataParseNews;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import sun.awt.SunHints;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -14,36 +14,37 @@ public class TestParserSpecialties {
 
     public static void main(String[] args) throws IOException {
 
+        List<List<String>> data = new ArrayList<>();
+        List<DataParsingSpecialties> dataParsingSpecialties = new ArrayList<>();
 
         Document doc = Jsoup.connect("http://priem.tti.sfedu.ru/index.php/kcp/bakalavriat-specialitet").get();
-        Elements trElements = doc.getElementsByAttributeValue("class","priemtable");
-        Elements rows=trElements.select("tr");
 
+        int count=0;
+        for (Element table : doc.select("table")) {
+            for (Element row : table.select("tr")) {
+                try {
+                    count++;
+                    if (row.select("td").eachText() != null&&count>=4&&count!=29&&count!=27)
 
-        for (Element table:doc.select("table")) {
-            for (Element row:table.select("tr")) {
-                Elements tds=row.select("td");
-                System.out.println(tds.get(0).text());
-               // if(!row.select("td").text().equals(row.select("td.kcp").text()))
-                //System.out.println(row.select("td").text());
+                        data.add(row.select("td").eachText());
+
+                   // System.out.println(row.select("td").eachText());
+
+                } catch (Exception e) {
+                    continue;
+                }
             }
-
         }
 
-
-
-//        trElements.forEach(trElement->{
-//            Element aElement=trElement;
-//            System.out.println(aElement.attr("href"));
-//            System.out.println(aElement.text());
-//        });
-
-//        pElements.forEach(pElement->{
-//            Element aElement =pElement;
-//            content.add(aElement.text());
-//
-//        });
-
+        for (int i = 3; i < data.size(); i++) {
+            if(i==26||i==28){}else {
+                dataParsingSpecialties.add(new DataParsingSpecialties(data.get(i).get(0), data.get(i).get(6),
+                        data.get(i).get(7), data.get(i).get(10)));
+            }
+           // System.out.println(data.get(i).get(0) + "  "+data.get(i).get(6)+" "+data.get(i).get(7)+" "+ data.get(i).get(10));}
+            //System.out.println(data.get(i).get(7));
+        }
+       dataParsingSpecialties.forEach(System.out::println);
     }
-
 }
+
